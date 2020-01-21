ef7b776b2158828265906c7dd88daf489416b517
mgomez-youngCoders <gomezmario619@yahoo.com>
2020-01-21 00:57:47 -0600

    Update Activity 2.py

--- Assignment 3/Activity 2.py
+++ Assignment 3/Activity 2.py
@@ -1,6 +1,3 @@
-#Activty 2 on Flowgorithm
-#This activity gives the user access to figure out how old they are by year.
-#It displays their age by year, month, day, hours, minutes, and seconds.
 print("Enter your age(in years): ")
 age = int(input())
 months = 12 * age
@@ -8,26 +5,4 @@ days = 365 * age
 hours = 8760 * age
 minutes = 525600 * age
 seconds = 31536000 * age
-print("You are")
-print(age)
-print("years old,")
-print(months)
-print("months old,")
-print(days)
-print("days old,")
-print(hours)
-print("hours old,")
-print(minutes)
-print("minutes old,")
-print(seconds)
-print("seconds old.")
-
-#Activity 2 on Repl.it
-#The code layout is different, but it works the same way.
-age = int(input("Enter your age(in years): "))
-months = 12 * age
-days = 365 * age
-hours = 8760 * age
-minutes = 525600 * age
-seconds = 31536000 * age
-print("You are", age, "years old,", months, "months old,", days, "days old,", hours, "hours old,", minutes, "minutes old,", seconds, "seconds old." )
+print("You are " + str(age) + "years old, " + str(months) + " months old, " + str(days) + " days old, " + str(hours) + " hours old, " + str(minutes) + "minutes old, " + str(seconds) + " seconds old.")
