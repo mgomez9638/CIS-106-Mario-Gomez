8ba0d231cdc395e1830618f7dec46836fe582f3e
mgomez-youngCoders <gomezmario619@yahoo.com>
2020-01-21 00:24:52 -0600

    Update Activity 1.py

--- Assignment 3/Activity 1.py
+++ Assignment 3/Activity 1.py
@@ -1,24 +1,10 @@
 #Assignment One in Flowgorithm
 #This program gives the user access to figure out how much they make weekly, monthly, and annually. 
 print("Enter hours worked: ")
-hours = int(input())
+hours = float(input())
 print("Enter hourly rate: ")
-rate = int(input())
+rate = float(input())
 weekly = rate * hours
 annually = 52 * weekly
-monthly = float(annually) / 12
-print("Your weekly earnings are $ ")
-print(weekly)
-print("Your monthly earnings are $ ")
-print(monthly)
-print("Your annual earnings are $ ")
-print(annually)
-
-#Assignment One in Repl.it 
-#The layout is a bit different, but it accomplishes the goal.
-hours = float(input("Enter hours worked: "))
-rate = float(input("Enter hourly rate: "))
-weekly = rate * hours
-annually = weekly * 52
 monthly = annually / 12
-print("Your weekly earnings are $", weekly, "your monthly earnings are $", monthly, "your annual earnings are $", annually)
+print("Your weekly earnings are $ " + str(weekly) + ".Your monthly earnings are $ " + str(monthly) + ".Your annually earnings are $ " + str(annually) + ".", end='', flush=True)
