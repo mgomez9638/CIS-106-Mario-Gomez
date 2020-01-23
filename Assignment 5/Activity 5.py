7094c561872c678786af6ba924519e44c4ac99c9
mgomez-youngCoders <gomezmario619@yahoo.com>
2020-01-23 02:15:53 -0600

    Update Activity 5.py

--- Assignment 5/Activity 5.py
+++ Assignment 5/Activity 5.py
@@ -1,1 +1,7 @@
-
+print("Enter the base(in feet): ")
+base = float(input())
+print("Enter the height(in feet): ")
+height = float(input())
+roomAreaInFeet = base * height
+roomAreaInYards = roomAreaInFeet / 3
+print("The amount of floor required is " + str(roomAreaInYards) + " sq. yds.")
