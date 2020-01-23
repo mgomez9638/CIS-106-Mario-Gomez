e156df3f2fae5e42ec09196192f6d02f6a6e7127
mgomez-youngCoders <gomezmario619@yahoo.com>
2020-01-23 02:42:55 -0600

    Update Activity 5.py

--- Assignment 5/Activity 5.py
+++ Assignment 5/Activity 5.py
@@ -1,7 +1,14 @@
+# Activity 5
+# It gives the user the power to calculate the amount of floor covering required.
+# The measurements start at linear feet and converts into square yards.
+
 print("Enter the base(in feet): ")
 base = float(input())
 print("Enter the height(in feet): ")
 height = float(input())
+
 roomAreaInFeet = base * height
 roomAreaInYards = roomAreaInFeet / 3
-print("The amount of floor required is " + str(roomAreaInYards) + " sq. yds.")
+
+print("The amount of floor required is " + str(roomAreaInYards) + 
+      " sq. yds.")
