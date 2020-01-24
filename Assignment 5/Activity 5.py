d95284e763160e865e00f1d3c4dcb0d8ae3af39b
mgomez-youngCoders <gomezmario619@yahoo.com>
2020-01-24 13:52:30 -0600

    Update Activity 5.py

--- Assignment 5/Activity 5.py
+++ Assignment 5/Activity 5.py
@@ -2,13 +2,34 @@
 # It gives the user the power to calculate the amount of floor covering required.
 # The measurements start at linear feet and converts into square yards.
 
-print("Enter the base(in feet): ")
-base = float(input())
-print("Enter the height(in feet): ")
-height = float(input())
+def calculateRoomAreaInFeet(base, height):
+    roomAreaInFeet = base * height
+    
+    return roomAreaInFeet
 
-roomAreaInFeet = base * height
-roomAreaInYards = roomAreaInFeet / 3
+def calculateRoomAreaInYards(roomAreaInFeet):
+    roomAreaInYards = roomAreaInFeet / 3
+    
+    return roomAreaInYards
 
-print("The amount of floor required is " + str(roomAreaInYards) + 
-      " sq. yds.")
+def displayResult(roomAreaInFeet, roomAreaInYards):
+    print("The amount of floor required is " + str(roomAreaInYards) + " square yards.")
+
+def getBase():
+    print("Enter the base(in feet): ")
+    base = float(input())
+    
+    return base
+
+def getHeight():
+    print("Enter the height(in feet): ")
+    height = float(input())
+    
+    return height
+
+# Main
+base = getBase()
+height = getHeight()
+roomAreaInFeet = calculateRoomAreaInFeet(base, height)
+roomAreaInYards = calculateRoomAreaInYards(roomAreaInFeet)
+displayResult(roomAreaInFeet, roomAreaInYards)
