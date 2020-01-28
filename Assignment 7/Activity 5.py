4b4d0438bb45eee2c39ceff99df79830c04814a2
mgomez-youngCoders <gomezmario619@yahoo.com>
2020-01-28 14:02:39 -0600

    Create Activity 5.py

---
+++ Assignment 7/Activity 5.py
@@ -0,0 +1,27 @@
+def calculatesockSize(shoeSize):
+    sockSize = int(shoeSize + 0.5)
+    
+    return sockSize
+
+def getShoeSize():
+    print("Enter your shoe size: ")
+    shoeSize = float(input())
+    
+    return shoeSize
+
+# Main
+shoeSize = getShoeSize()
+sockSize = calculatesockSize(shoeSize)
+if sockSize < 4:
+    print("Your sock size is extra small.")
+else:
+    if 4 <= sockSize and sockSize <= 6:
+        print("You sock size is small.")
+    else:
+        if 7 <= sockSize and sockSize <= 9:
+            print("Your sock size is medium.")
+        else:
+            if 10 <= sockSize and sockSize <= 12:
+                print("You sock size is large.")
+            else:
+                print("Your sock size is extra large.")
