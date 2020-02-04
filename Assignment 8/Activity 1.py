ada700784bfd1aaa36bd2f9253c3e19b13c885ce
mgomez-youngCoders <gomezmario619@yahoo.com>
2020-02-04 02:49:40 -0600

    Update Activity 1.py

--- Assignment 8/Activity 1.py
+++ Assignment 8/Activity 1.py
@@ -2,31 +2,23 @@
 # This program gives the user access to create a multiplication table.
 # You simply begin with entering a value, entering a starting point, and the size of the table.
 
-def getMultiplierValue():
-    print("Enter the starting point value: ")
-    multiplierValue = int(input())
+def getExpressions():
+    print("Enter the number of expressions")
+    expressions = int(input())
     
-    return multiplierValue
+    return expressions
 
-def getSize():
-    print("Enter the size of the table(larger than the value): ")
-    size = int(input())
-    
-    return size
-
-def getStableValue():
+def getValue():
     print("Enter a value: ")
-    stableValue = int(input())
+    value = int(input())
     
-    return stableValue
+    return value
 
 # Main
-stableValue = getStableValue()
-multiplierValue = getMultiplierValue()
-size = getSize()
-while stableValue <= size:
-    while multiplierValue <= size:
-        total = stableValue * multiplierValue
-        print(str(stableValue) + " * " + str(multiplierValue) + " = " + str(total))
-        multiplierValue = multiplierValue + 1
-stableValue = stableValue + 1
+value = getValue()
+expressions = getExpressions()
+multiplierValue = 1
+while multiplierValue <= expressions:
+    total = value * multiplierValue
+    print(str(value) + " * " + str(multiplierValue) + " = " + str(total))
+    multiplierValue = multiplierValue + 1
