11e591e2a6d3929f41ba79492c26856a5498c3d9
mgomez-youngCoders <gomezmario619@yahoo.com>
2020-02-07 05:46:44 -0600

    Update Activity 3.py

--- Assignment 10/Activity 3.py
+++ Assignment 10/Activity 3.py
@@ -2,38 +2,24 @@
 # This program gives the user access to figure out how much they make weekly, monthly, and annually.
 # The user has the option to run the amount of money they would be making with different hourly rates.
 
-
-def getAnotherGo():
-    print("Would you like to go again? ")
-    anotherGo = input()
+           
+def getExpressions():
+    print("Enter the number of expressions: ")
+    expressions = int(input())
     
-    return anotherGo
+    return expressions
 
-def getHours():
-    print("Enter hours worked: ")
-    hours = float(input())
+def getValue():
+    print("Enter a value: ")
+    value = int(input())
     
-    return hours
-
-def getRate():
-    print("Enter hourly rate of pay: ")
-    rate = float(input())
-    
-    return rate
+    return value
 
 # Main
-pay = True
-while pay == True:
-    hours = getHours()
-    rate = getRate()
-    weekly = rate * hours
-    annually = 52 * weekly
-    monthly = annually / 12
-    print("This pay is before taxes. Your weekly earnings are $" + str(weekly) + ". Your monthly earnings are $" + str(monthly) + ". Your annually earnings are $" + str(annually) + ".")
-    goAgain = True
-    while goAgain == True:
-        anotherGo = getAnotherGo()
-        if anotherGo == "yes" and anotherGo == "Yes" and anotherGo == "y" and anotherGo == "Y":
-            goAgain = True
-        else:
-            goAgain = False
+value = getValue()
+expressions = getExpressions()
+multiplierValue = 1
+for multiplierValue in range(1, expressions + 1, 1):
+    total = value * multiplierValue
+    print(str(value) + " * " + str(multiplierValue) + " = " + str(total))
+    multiplierValue = multiplierValue + 1
