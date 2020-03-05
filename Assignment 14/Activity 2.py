d951c039c1cc281d271adc3934395e2662bc2dd3
mgomez-youngCoders <gomezmario619@yahoo.com>
2020-03-05 01:40:00 -0600

    Update Activity 2.py

--- Assignment 14/Activity 2.py
+++ Assignment 14/Activity 2.py
@@ -1,25 +1,14 @@
-# Activity 2
-# This program implements a text file to be able to read information.
-# It displays all the scores from the file.
-# The program intents to display the high score, low score, and average score.
-# This program includes error handling.
-
-
 import os
 import sys
 
 
 # check if text file exists
 def file_exist():
-    try:
-        my_file = "scores.txt"
-        if os.path.isfile(my_file):
-            print("Processing....\n")
-            print("File does exist!\n")
-            return my_file
-    except FileNotFoundError:
-        print("File is missing", my_file)
-        print(sys.exc_info()[1])
+    my_file = "scores.txt"
+    if os.path.isfile(my_file):
+        print("Processing....\n")
+        print("File does exist!\n")
+        return my_file
 
 
 # read the text file
@@ -33,41 +22,79 @@ def get_file(my_file):
                 text = line.split(",")
                 scores.append(float(text[1]))
             except ValueError:
-                print("Incorrect data", my_file)
+                print("Missing data", my_file)
+                print(sys.exc_info()[1])
+                pass
+            except IndexError:
+                print("Incorrectly formatted", my_file)
                 print(sys.exc_info()[1])
         my_file.close()
         return scores
-    except Exception:
-        print("Error reading the format", my_file)
+    except FileNotFoundError:
+        print("Missing file", my_file)
         print(sys.exc_info()[1])
-    if not my_file:
-        print("File is empty!")
 
 
 # high score
-def get_high(scores):
-    high = max(scores)
-    return high
+def get_high(my_file, scores):
+    try:
+        high = max(scores)
+        return high
+    except TypeError:
+        print("Wrong type", my_file)
+        print(sys.exc_info()[1])
+        pass
+    except ValueError:
+        print("Missing data", my_file)
+        print(sys.exc_info()[1])
+        pass
 
 
 # low score
-def get_low(scores):
-    low = min(scores)
-    return low
+def get_low(my_file, scores):
+    try:
+        low = min(scores)
+        return low
+    except TypeError:
+        print("Wrong type", my_file)
+        print(sys.exc_info()[1])
+        pass
+    except ValueError:
+        print("Missing data", my_file)
+        print(sys.exc_info()[1])
+        pass
 
 
 # average score
-def get_average(scores):
-    total = sum(scores)
-    average = round(total/len(scores), 2)
-    return average
+def get_average(my_file, scores):
+    try:
+        total = sum(scores)
+        average = round(total/len(scores), 2)
+        return average
+    except ZeroDivisionError:
+        print("Missing values", my_file)
+        print(sys.exc_info()[1])
+        pass
+    except ValueError:
+        print("Missing data", my_file)
+        print(sys.exc_info()[1])
+        pass
+    except TypeError:
+        print("Wrong type", my_file)
+        print(sys.exc_info()[1])
+        pass
 
 
 # displays scores
-def display_result(high, low, average):
-    print("The highest score is: ", high)
-    print("The lowest score is: ", low)
-    print("The average score is: ", average)
+def display_result(my_file, high, low, average):
+    try:
+        print("The highest score is: ", high)
+        print("The lowest score is: ", low)
+        print("The average score is: ", average)
+    except ValueError:
+        print("Missing data", my_file)
+        print(sys.exc_info()[1])
+        pass
 
 
 # main function
@@ -75,10 +102,10 @@ def main():
     print("\t\tWelcome to Programming with Files!\n\n")
     my_file = file_exist()
     scores = get_file(my_file)
-    high = get_high(scores)
-    low = get_low(scores)
-    average = get_average(scores)
-    display_result(high, low, average)
+    high = get_high(my_file, scores)
+    low = get_low(my_file, scores)
+    average = get_average(my_file, scores)
+    display_result(my_file, high, low, average)
     print("\n\n\t\tThank You for Programming with Files!\n\n")
 
 
