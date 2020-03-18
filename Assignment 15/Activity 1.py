7e7e3bc73783ac26f4c7bde3c499e2e54389bc69
Dave Braunschweig <dave@chicagocoding.com>
2020-03-18 09:19:37 -0500

    Assignment 15 corrections

--- Assignment 15/Activity 1.py
+++ Assignment 15/Activity 1.py
@@ -6,7 +6,33 @@
 # Error handling has been included.
 
 
+import math
 import sys
+
+
+class Shape_Area():
+    def area_triangle(self, base, height):
+        return base * height / 2
+
+
+    def area_square(self, length):
+        return length ** 2
+
+
+    def area_rectangle(self, width, height):
+        return width * height
+
+
+    def area_parallelogram(self, base, height):
+        return base * height
+
+
+    def area_trapezoid(self, side, base):
+        return (side + base) / 2
+
+
+    def area_circle(self, radius):
+        return math.pi * radius ** 2
 
 
 def get_choice():
@@ -17,11 +43,10 @@ def get_choice():
 def shape_triangle():
     try:
         base = float(input("Enter the base: "))
-        vertical_height = float(input("Enter the vertical height: "))
-        length_of_sides = 0
-        width = 0
-        radius = 0
-        return base, vertical_height, length_of_sides, width, radius
+        height = float(input("Enter the vertical height: "))
+        shape = Shape_Area()
+        area = shape.area_triangle(base, height)
+        return area
     except TypeError:
         print("You need to enter a number!")
         print(sys.exc_info()[1])
@@ -32,12 +57,10 @@ def shape_triangle():
 
 def shape_square():
     try:
-        base = 0
-        vertical_height = 0
-        length_of_sides = float(input("Enter the length of a side: "))
-        width = 0
-        radius = 0
-        return base, vertical_height, length_of_sides, width, radius
+        side = float(input("Enter the length of a side: "))
+        shape = Shape_Area()
+        area = shape.area_square(side)
+        return area
     except TypeError:
         print("You need to enter a number!")
         print(sys.exc_info()[1])
@@ -48,12 +71,11 @@ def shape_square():
 
 def shape_rectangle():
     try:
-        base = 0
-        vertical_height = float(input("Enter the vertical height: "))
-        length_of_sides = 0
+        height = float(input("Enter the vertical height: "))
         width = float(input("Enter the width: "))
-        radius = 0
-        return base, vertical_height, length_of_sides, width, radius
+        shape = Shape_Area()
+        area = shape.area_rectangle(width, height)
+        return area
     except TypeError:
         print("You need to enter a number!")
         print(sys.exc_info()[1])
@@ -65,11 +87,10 @@ def shape_rectangle():
 def shape_parallelogram():
     try:
         base = float(input("Enter the base: "))
-        vertical_height = float(input("Enter the vertical height: "))
-        length_of_sides = 0
-        width = 0
-        radius = 0
-        return base, vertical_height, length_of_sides, width, radius
+        height = float(input("Enter the vertical height: "))
+        shape = Shape_Area()
+        area = shape.area_parallelogram(base, height)
+        return area
     except TypeError:
         print("You need to enter a number!")
         print(sys.exc_info()[1])
@@ -81,11 +102,10 @@ def shape_parallelogram():
 def shape_trapezoid():
     try:
         base = float(input("Enter the base: "))
-        vertical_height = 0
-        length_of_sides = float(input("Enter the length of a side: "))
-        width = 0
-        radius = 0
-        return base, vertical_height, length_of_sides, width, radius
+        side = float(input("Enter the length of a side: "))
+        shape = Shape_Area()
+        area = shape.area_trapezoid(side, base)
+        return area
     except TypeError:
         print("You need to enter a number!")
         print(sys.exc_info()[1])
@@ -96,12 +116,10 @@ def shape_trapezoid():
 
 def shape_circle():
     try:
-        base = 0
-        vertical_height = 0
-        length_of_sides = 0
-        width = 0
         radius = float(input("Enter the radius: "))
-        return base, vertical_height, length_of_sides, width, radius
+        shape = Shape_Area()
+        area = shape.area_circle(radius)
+        return area
     except TypeError:
         print("You need to enter a number!")
         print(sys.exc_info()[1])
@@ -110,66 +128,27 @@ def shape_circle():
         print(sys.exc_info()[1])
 
 
-class shape_area():
-    def __init__(self, base, vertical_height, length_of_sides, width, radius):
-        self.base = base
-        self.vertical_height = vertical_height
-        self.length_of_sides = length_of_sides
-        self.width = width
-        self.radius = radius
-
-
-    def area_triangle(self):
-        return self.base * self.vertical_height * (1/2)
-
-
-    def area_square(self):
-        return self.length_of_sides ** 2
-
-
-    def area_rectangle(self):
-        return self.width * self.vertical_height
-
-
-    def area_parallelogram(self):
-        return self.base * self.vertical_height
-
-
-    def area_trapezoid(self):
-        return (self.length_of_sides + self.base) * (1/2)
-
-
-    def area_circle(self):
-        return (self.radius ** 2) * 3.14
-
-
 def main():
     print("\t\tWelcome to the Shape Area Program!\n\n")
     choice = get_choice()
     if (choice == "T" or choice == "t"):
-        base, vertical_height, length_of_sides, width, radius = shape_triangle()
-        shape = shape_area(base, vertical_height, length_of_sides, width, radius)
-        print("The area of a triangle is ", round(shape.area_triangle(), 2))
+        area = shape_triangle()
+        print("The area of a triangle is ", round(area, 2))
     elif (choice == "S" or choice == "s"):
-        base, vertical_height, length_of_sides, width, radius = shape_square()
-        shape = shape_area(base, vertical_height, length_of_sides, width, radius)
-        print("The area of a square is ", round(shape.area_square(), 2))
+        area = shape_square()
+        print("The area of a square is ", round(area, 2))
     elif (choice == "R" or choice == "r"):
-        base, vertical_height, length_of_sides, width, radius = shape_rectangle()
-        shape = shape_area(base, vertical_height, length_of_sides, width, radius)
-        print("The area of a rectangle is ", round(shape.area_rectangle(), 2))
+        area = shape_rectangle()
+        print("The area of a rectangle is ", round(area, 2))
     elif (choice == "P" or choice == "p"):
-        base, vertical_height, length_of_sides, width, radius = shape_parallelogram()
-        shape = shape_area(base, vertical_height, length_of_sides, width, radius)
-        print("The area of a parallelogram is ", round(shape.area_parallelogram(), 2))
+        area = shape_parallelogram()
+        print("The area of a parallelogram is ", round(area, 2))
     elif (choice == "Tr" or choice == "tr"):
-        base, vertical_height, length_of_sides, width, radius = shape_trapezoid()
-        shape = shape_area(base, vertical_height, length_of_sides, width, radius)
-        print("The area of a trapezoid is ", round(shape.area_trapezoid(), 2))
+        area = shape_trapezoid()
+        print("The area of a trapezoid is ", round(area, 2))
     elif (choice == "C" or choice == "c"):
-        base, vertical_height, length_of_sides, width, radius = shape_circle()
-        shape = shape_area(base, vertical_height, length_of_sides, width, radius)
-        print("The area of a circle is ", round(shape.area_circle(), 2))
+        area = shape_circle()
+        print("The area of a circle is ", round(area, 2))
     else:
         raise Exception("You must enter T for triangle, S for square, R for rectangle, P for parallelogram, TR for trapezoid or C for circle!", sys.exc_info()[1])
     print("\n\n\t\tThank you for using the Shape Area Program!")
