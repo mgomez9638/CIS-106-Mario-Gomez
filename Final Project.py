dbfc70215f247a96d7e70bf52d6daa2ad247bc25
mgomez-youngCoders <gomezmario619@yahoo.com>
2020-03-28 02:30:31 -0500

    Update Final Project.py

--- Final Project.py
+++ Final Project.py
@@ -23,3 +23,38 @@ def find_xml():
         print("URL information is incorrect!")
         print(sys.exc_info()[1])
  
+
+def get_xml_info(xml_file):
+    title = []
+    artist = []
+    country = []
+    price = []
+    year = []
+    music = xml.etree.ElementTree.parse(xml_file)
+    subject = music.getroot()
+    try:
+        for cd in subject.findall("CD"):
+            titles = cd.find("TITLE").text
+            title.append(titles)
+            artists = cd.find("ARTIST").text
+            artist.append(artists)
+            countries = cd.find("COUNTRY").text
+            country.append(countries)
+            prices = cd.find("PRICE").text
+            price.append(float(prices))
+            years = cd.find("YEAR").text
+            year.append(years)
+        return title, artist, country, price, year
+    except NameError:
+        print("Incorrect Name", xml_file)
+        print(sys.exc_info()[1])
+    except AttributeError:
+        print("Wrong Type", xml_file)
+        print(sys.exc_info()[1])
+    except IndexError:
+        print("Incorrectly formatted", xml_file)
+        print(sys.exc_info()[1])
+    except ValueError:
+        print("Missing data", xml_file)
+        print(sys.exc_info()[1])
+ 
