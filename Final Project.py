d576246e15c00d87b1ba6d0710d38e0597c53cbb
mgomez-youngCoders <gomezmario619@yahoo.com>
2020-03-28 01:32:02 -0500

    Update Final Project.py

--- Final Project.py
+++ Final Project.py
@@ -1,1 +1,6 @@
-
+# Final Project
+# This program reads data from a url using Internet processing methods and builds arrays for each indentation using XML functions.
+# Afterwards, it displays the catalog items, the amount of items, and the average price of the items.
+#
+# References:
+#     https://docs.python.org/3/py-modindex.html
