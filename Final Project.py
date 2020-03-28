ddb7f8e1aba6493cef6fde86c9744a9d739ad025
mgomez-youngCoders <gomezmario619@yahoo.com>
2020-03-28 01:59:26 -0500

    Update Final Project.py

--- Final Project.py
+++ Final Project.py
@@ -4,3 +4,22 @@
 #
 # References:
 #     https://docs.python.org/3/py-modindex.html
+
+
+import sys
+import urllib.request
+import urllib.error
+
+
+def find_xml():
+    try:
+        url = "https://www.w3schools.com/xml/cd_catalog.xml"
+        xml_file = urllib.request.urlopen(url)
+        return xml_file
+    except urllib.error.HTTPError:
+        print("URL is incorrect!")
+        print(sys.exc_info()[1])
+    except urrlib.error.URLError:
+        print("URL information is incorrect!")
+        print(sys.exc_info()[1])
+ 
